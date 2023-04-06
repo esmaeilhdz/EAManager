a2c03c7e0b009087e7f4cc6d124d63172b6f61e1
esmaeil <heydarzadeh.design@gmail.com>
2023-04-06 10:46:31 +0330

    sale period permission

--- routes/sale_period.php
+++ routes/sale_period.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\SalePeriodController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
-    Route::group(['middleware' => ['permission:admin-sale_period|edit-sale_period|view-sale_period']], function () {
+    Route::group(['middleware' => ['permission:admin-sale_periods|edit-sale_periods|view-sale_periods']], function () {
         Route::get('sale_period', [SalePeriodController::class, 'getSalePeriods']);
         Route::get('sale_period/{id}', [SalePeriodController::class, 'getSalePeriodDetail']);
     });
-    Route::group(['middleware' => ['permission:admin-sale_period|edit-sale_period']], function () {
+    Route::group(['middleware' => ['permission:admin-sale_periods|edit-sale_periods']], function () {
         Route::put('sale_period/{id}', [SalePeriodController::class, 'editSalePeriod']);
     });
-    Route::group(['middleware' => ['permission:admin-sale_period']], function () {
+    Route::group(['middleware' => ['permission:admin-sale_periods']], function () {
         Route::post('sale_period', [SalePeriodController::class, 'addSalePeriod']);
         Route::delete('sale_period/{id}', [SalePeriodController::class, 'deleteSalePeriod']);
     });
