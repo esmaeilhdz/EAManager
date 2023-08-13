52e6e5d2f0bad190bd15f6a1575c09a3a6e993d3
esmaeil <heydarzadeh.design@gmail.com>
2023-08-13 20:28:25 +0330

    chore: remove branch_name from account

---
+++ database/migrations/2023_08_13_202125_drop_column_accounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropColumnAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('branch_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
