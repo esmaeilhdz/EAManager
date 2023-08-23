d0ebde9ba1684979f849a4c5c52b625c7abff29a
esmaeil <heydarzadeh.design@gmail.com>
2023-08-23 19:28:23 +0330

    bugfix

--- app/Helpers/CustomerHelper.php
+++ app/Helpers/CustomerHelper.php
@@ -41,8 +41,8 @@ class CustomerHelper
                 'name' => $item->name,
                 'mobile' => $item->mobile,
                 'score' => $item->score,
-                'province' => $item->address[0]->province->name,
-                'city' => $item->address[0]->city->name,
+                'province' => $item->address[0]->province->name ?? null,
+                'city' => $item->address[0]->city->name ?? null,
                 'creator' => is_null($item->creator->person) ? null : [
                     'person' => [
                         'full_name' => $item->creator->person->name . ' ' . $item->creator->person->family,
